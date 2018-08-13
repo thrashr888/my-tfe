22302b3b56151d1bbaab2812fea6bf9c5721c540
Paul Thrasher <thrashr888@gmail.com>
2018-08-13 11:59:04 -0700

    add variables + descriptions

--- main.tf
+++ main.tf
@@ -1,7 +1,31 @@
 variable "hostname" {
   default="app.terraform.io"
+  description="The server that TFE is running on"
 }
 variable "TFE_TOKEN" {}
+
+
+variable "organization_name" {
+  default="pthrasher-main"
+  description="The name of the TFE Organization"
+}
+variable "organization_email" {
+  default="pthrasher@hashicorp.com"
+  description="The email address of the TFE Organization"
+}
+
+variable "workspace_name" {
+  default="my-tfe"
+  description="The name of the TFE Workspace"
+}
+variable "repo_identifier" {
+  default="thrashr888/my-tfe"
+  description="The name of the repo to connect the TFE Workspace to, ex. \"thrashr888/my-tfe\""
+}
+variable "oauth_token_id" {
+  default="ot-Dokh8KgR6rbXV9es"
+  description="The `token_id` of the OAuth connection to the TFE Workspace. It is provided once you configure an OAuth connection in the TFE Organization."
+}
 
 
 # Configure the Terraform Enterprise Provider
@@ -11,17 +35,17 @@ provider "tfe" {
 }
 
 resource "tfe_organization" "organization" {
-  name = "pthrasher-main"
-  email = "pthrasher@hashicorp.com"
+  name = "${var.organization_name}"
+  email = "${var.organization_email}"
 }
 
 
 resource "tfe_workspace" "workspace" {
-  name = "my-tfe"
+  name = "${var.workspace_name}"
   organization = "${tfe_organization.organization.id}"
   vcs_repo = {
-    identifier = "thrashr888/my-tfe" # run thyself!
-    oauth_token_id = "ot-Dokh8KgR6rbXV9es"
+    identifier = "${var.repo_identifier}"
+    oauth_token_id = "${var.oauth_token_id}"
     branch = "master"
   }
 }
