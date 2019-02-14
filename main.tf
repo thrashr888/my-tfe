56206406df242229f88efacac6bbf9f41a144cf9
Paul Thrasher <thrashr888@gmail.com>
2019-02-13 17:02:23 -0800

    fmt

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
 variable "hostname" {
-  default="app.terraform.io"
-  description="The server that TFE is running on"
+  default     = "app.terraform.io"
+  description = "The server that TFE is running on"
 }
 variable "TFE_TOKEN" {}
 
 
 variable "organization_name" {
-  default="pthrasher-main"
-  description="The name of the TFE Organization"
+  default     = "pthrasher-main"
+  description = "The name of the TFE Organization"
 }
 variable "organization_email" {
-  default="pthrasher@hashicorp.com"
-  description="The email address of the TFE Organization"
+  default     = "pthrasher@hashicorp.com"
+  description = "The email address of the TFE Organization"
 }
 
 variable "workspace_name" {
-  default="my-tfe"
-  description="The name of the TFE Workspace"
+  default     = "my-tfe"
+  description = "The name of the TFE Workspace"
 }
 variable "repo_identifier" {
-  default="thrashr888/my-tfe"
-  description="The name of the repo to connect the TFE Workspace to, ex. \"thrashr888/my-tfe\""
+  default     = "thrashr888/my-tfe"
+  description = "The name of the repo to connect the TFE Workspace to, ex. \"thrashr888/my-tfe\""
 }
 variable "oauth_token_id" {
-  default="ot-Dokh8KgR6rbXV9es"
-  description="The `token_id` of the OAuth connection to the TFE Workspace. It is provided once you configure an OAuth connection in the TFE Organization."
+  default     = "ot-Dokh8KgR6rbXV9es"
+  description = "The `token_id` of the OAuth connection to the TFE Workspace. It is provided once you configure an OAuth connection in the TFE Organization."
 }
 
 
@@ -35,25 +35,25 @@ provider "tfe" {
 }
 
 resource "tfe_organization" "organization" {
-  name = "${var.organization_name}"
+  name  = "${var.organization_name}"
   email = "${var.organization_email}"
 }
 
 
 resource "tfe_workspace" "workspace" {
-  name = "${var.workspace_name}"
+  name         = "${var.workspace_name}"
   organization = "${tfe_organization.organization.id}"
   vcs_repo = {
-    identifier = "${var.repo_identifier}"
+    identifier     = "${var.repo_identifier}"
     oauth_token_id = "${var.oauth_token_id}"
-    branch = "master"
+    branch         = "master"
   }
 }
 
 resource "tfe_variable" "variable" {
-  key = "TF_VAR_TFE_TOKEN"
-  value = "${var.TFE_TOKEN}"
-  category = "env"
+  key          = "TF_VAR_TFE_TOKEN"
+  value        = "${var.TFE_TOKEN}"
+  category     = "env"
   workspace_id = "${tfe_workspace.workspace.id}"
-  sensitive = true
+  sensitive    = true
 }
